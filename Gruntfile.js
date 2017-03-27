94f31e0c5cbcfedb3ed53abc874eeb0a5037d321
Ambrose Chua <ambrose@chua.family>
2017-03-27 21:42:01 +0800

    Move to react-toolbox

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,24 +4,54 @@ module.exports = function (grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		webpack: {
 			app: {
-				entry: './app/index.jsx',
+				entry: __dirname + '/app/index.jsx',
 				output: {
 					filename: 'bundle.js',
-					path: './dist/app/'
+					path: __dirname + '/dist/app/'
 				},
 				resolve: {
 					extensions: ['.js', '.jsx'],
 				},
 				module: {
-					loaders: [
+					rules: [
 						{
 							test: /\.jsx?$/,
 							exclude: /node_modules/,
-							loader: 'babel-loader',
-							query: {
-								sourceMap: true,
-								presets: ['env', 'react']
-							}
+							use: [
+								{
+									loader: 'babel-loader',
+									options: {
+										sourceMap: true,
+										presets: ['env', 'react']
+									}
+								}
+							]
+						},
+						{
+							test: /\.css$/,
+							use: [
+								'style-loader',
+								{
+									loader: 'css-loader',
+									options: {
+										modules: true,
+										importLoaders: 1
+									}
+								},
+								{
+									loader: 'postcss-loader',
+									options: {
+										plugins: {
+											'postcss-import': {
+												root: __dirname,
+											},
+											'postcss-mixins': {},
+											'postcss-each': {},
+											'postcss-cssnext': {}
+										}
+									}
+								}
+							]
 						}
 					]
 				}
@@ -32,27 +62,21 @@ module.exports = function (grunt) {
 				files: [
 					{
 						expand: true,
-						cwd: './app/',
+						cwd: __dirname + '/app/',
 						src: ['index.html'],
-						dest: './dist/app/'
+						dest: __dirname + '/dist/app/'
 					},
 					{
 						expand: true,
-						cwd: './app/',
+						cwd: __dirname + '/app/',
 						src: ['assets/**'],
-						dest: './dist/app/assets/'
+						dest: __dirname + '/dist/app/assets/'
 					},
 					{
 						expand: true,
-						cwd: './node_modules/bootstrap/',
+						cwd: __dirname + '/node_modules/oidc-client/',
 						src: ['dist/**'],
-						dest: './dist/app/assets/bootstrap/'
-					},
-					{
-						expand: true,
-						cwd: './node_modules/oidc-client/',
-						src: ['dist/**'],
-						dest: './dist/app/assets/oidc-client/'
+						dest: __dirname + '/dist/app/assets/oidc-client/'
 					}
 				]
 			}
@@ -72,9 +96,9 @@ module.exports = function (grunt) {
 				files: [
 					{
 						expand: true,
-						cwd: './server/',
+						cwd: __dirname + '/server/',
 						src: ['**/*.js'],
-						dest: './dist/server/'
+						dest: __dirname + '/dist/server/'
 					}
 				]
 			}
@@ -84,9 +108,9 @@ module.exports = function (grunt) {
 				files: [
 					{
 						expand: true,
-						cwd: './app/',
+						cwd: __dirname + '/app/',
 						src: ['**/*.js*'],
-						dest: './dist/app/'
+						dest: __dirname + '/dist/app/'
 					}
 				]
 			},
@@ -94,9 +118,9 @@ module.exports = function (grunt) {
 				files: [
 					{
 						expand: true,
-						cwd: './server/',
+						cwd: __dirname + '/server/',
 						src: ['**/*.js'],
-						dest: './dist/server/'
+						dest: __dirname + '/dist/server/'
 					}
 				]
 			}
